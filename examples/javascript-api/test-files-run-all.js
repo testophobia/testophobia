842158a4b35d85f91d7d2cc95e6f84aac8ea0ff5
Jason Gardner <jason.gardner.lv@gmail.com>
2018-10-23 11:06:40 -0700

    some example files for testing the javascript api

---
+++ examples/javascript-api/test-files-run-all.js
@@ -0,0 +1,11 @@
+const {Testophobia} = require('../../lib/testophobia');
+
+/* This test sets up multiple tests from a file glob, and then runs them all */
+const tp = new Testophobia({
+  fileType: "jpeg",
+  golden: false,
+  baseUrl: 'https://testophobia.github.io/testophobia/examples/basic',
+  tests: 'tests/**/*-test.js'
+});
+
+tp.run();
---
+++ examples/javascript-api/tests/about/about-test.js
@@ -0,0 +1,4 @@
+export default {
+  name: "about",
+  path: "about/about.html"
+};
