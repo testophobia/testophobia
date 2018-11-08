3f93f49be62f78b0e384048cd263ef51ad2b3b54
Jason Gardner <jason.gardner.lv@gmail.com>
2018-11-08 09:55:12 -0800

    recorder element selector updates

--- experimental/recorder/content-script.js
+++ experimental/recorder/content-script.js
@@ -3,61 +3,119 @@
   https://stackoverflow.com/a/44305325
 */
 
+//TODO - still need to figure out how to import this library properly
 function querySelectorDeep(selector, findMany) {
-  let lightElement = document.querySelector(selector);
-  if (document.head.createShadowRoot || document.head.attachShadow) {
-    if (!findMany && lightElement) return lightElement;
-    const splitSelector = selector.replace(/\s*([,>+~]+)\s*/g, '$1').split(' ');
-    const possibleElementsIndex = splitSelector.length - 1;
-    const possibleElements = collectAllElementsDeep(splitSelector[possibleElementsIndex]);
-    const findElements = findMatchingElement(splitSelector, possibleElementsIndex);
-    if (findMany) {
-      return possibleElements.filter(findElements);
+    let lightElement = document.querySelector(selector);
+
+    if (document.head.createShadowRoot || document.head.attachShadow) {
+        // no need to do any special if selector matches something specific in light-dom
+        if (!findMany && lightElement) {
+            return lightElement;
+        }
+
+        // split on commas because those are a logical divide in the operation
+        const selectionsToMake = selector.split(/,\s*/);
+
+        return selectionsToMake.reduce((acc, minimalSelector) => {
+            // if not finding many just reduce the first match
+            if (!findMany && acc) {
+                return acc;
+            }
+            // do best to support complex selectors and split the query
+            const splitSelector = minimalSelector
+                //remove white space at start of selector
+                .replace(/^\s+/g, '')
+                .replace(/\s*([>+~]+)\s*/g, '$1')
+                .replace(/\s\s+/g, ' ')
+                // split on space unless in quotes
+                .match(/\\?.|^$/g).reduce((p, c) => {
+                    if (c === '"' && !p.sQuote) {
+                        p.quote ^= 1;
+                        p.a[p.a.length - 1] += c;
+                    } else if (c === '\'' && !p.quote) {
+                        p.sQuote ^= 1;
+                        p.a[p.a.length - 1] += c;
+
+                    } else if (!p.quote && !p.sQuote && c === ' ') {
+                        p.a.push('');
+                    } else {
+                        p.a[p.a.length - 1] += c;
+                    }
+                    return p;
+                }, { a: [''] }).a;
+            const possibleElementsIndex = splitSelector.length - 1;
+            const possibleElements = collectAllElementsDeep(splitSelector[possibleElementsIndex]);
+            const findElements = findMatchingElement(splitSelector, possibleElementsIndex);
+            if (findMany) {
+                acc = acc.concat(possibleElements.filter(findElements));
+                return acc;
+            } else {
+                acc = possibleElements.find(findElements);
+                return acc;
+            }
+        }, findMany ? [] : null);
+
+
     } else {
-      return possibleElements.find(findElements);
+        if (!findMany) {
+            return lightElement;
+        } else {
+            return document.querySelectorAll(selector);
+        }
     }
-  } else {
-    if (!findMany) {
-      return lightElement;
-    } else {
-      return document.querySelectorAll(selector);
-    }
-  }
+
 }
 
 function findMatchingElement(splitSelector, possibleElementsIndex) {
-  return (element) => {
-    let position = possibleElementsIndex;
-    let parent = element;
-    let foundElement = false;
-    while (parent) {
-      const foundMatch = parent.matches(splitSelector[position]);
-      if (foundMatch && position === 0) {
-        foundElement = true;
-        break;
-      }
-      if (foundMatch) position--;
-      parent = findParentOrHost(parent);
-    }
-    return foundElement;
-  };
+    return (element) => {
+        let position = possibleElementsIndex;
+        let parent = element;
+        let foundElement = false;
+        while (parent) {
+            const foundMatch = parent.matches(splitSelector[position]);
+            if (foundMatch && position === 0) {
+                foundElement = true;
+                break;
+            }
+            if (foundMatch) {
+                position--;
+            }
+            parent = findParentOrHost(parent);
+        }
+        return foundElement;
+    };
+
 }
+
 
 function findParentOrHost(element) {
-  const parentNode = element.parentNode;
-  return (parentNode && parentNode.nodeType === 11 && parentNode.host) ? parentNode.host : parentNode === document ? null : parentNode;
+    const parentNode = element.parentNode;
+    return (parentNode && parentNode.host && parentNode.nodeType === 11) ? parentNode.host : parentNode === document ? null : parentNode;
 }
 
+/**
+ * Finds all elements on the page, inclusive of those within shadow roots.
+ * @param {string=} selector Simple selector to filter the elements by. e.g. 'a', 'div.main'
+ * @return {!Array<string>} List of anchor hrefs.
+ * @author ebidel@ (Eric Bidelman)
+ * License Apache-2.0
+ */
 function collectAllElementsDeep(selector = null) {
-  const allElements = [];
-  const findAllElements = function(nodes) {
-    for (let i = 0, el; el = nodes[i]; ++i) { //eslint-disable-line no-cond-assign
-      allElements.push(el);
-      if (el.shadowRoot) findAllElements(el.shadowRoot.querySelectorAll('*'));
-    }
-  };
-  findAllElements(document.querySelectorAll('*'));
-  return selector ? allElements.filter(el => el.matches(selector)) : allElements;
+    const allElements = [];
+
+    const findAllElements = function(nodes) {
+        for (let i = 0, el; el = nodes[i]; ++i) { //eslint-disable-line no-cond-assign
+            allElements.push(el);
+            // If the element has a shadow root, dig deeper.
+            if (el.shadowRoot) {
+                findAllElements(el.shadowRoot.querySelectorAll('*'));
+            }
+        }
+    };
+
+    findAllElements(document.querySelectorAll('*'));
+
+    return selector ? allElements.filter(el => el.matches(selector)) : allElements;
 }
 
 function getUniqueSelector(elSrc) { //eslint-disable-line no-unused-vars
@@ -90,8 +148,8 @@ function getUniqueSelector(elSrc) { //eslint-disable-line no-unused-vars
       }
     }
     // 3. Try to select by classes
-    if (el.className) {
-      aSel[0] = sSel += '.' + el.className.trim().replace(/ +/g, '.');
+    if (el.getAttribute('class')) {
+      aSel[0] = sSel += '.' + el.getAttribute('class').trim().replace(/ +/g, '.');
       if (uniqueQuery()) {
         return true;
       }
@@ -124,6 +182,7 @@ function getUniqueSelector(elSrc) { //eslint-disable-line no-unused-vars
       break;
     }
     elSrc = findParentOrHost(elSrc);
+
   }
   return returnVal;
 }
