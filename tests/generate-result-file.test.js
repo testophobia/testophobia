85e99c70d551f7e384e715e221ccd38c99648450
srmward <srmward@gmail.com>
2018-11-21 12:38:46 -0800

    Unit Tests: Generate Results File

---
+++ tests/generate-result-file.test.js
@@ -0,0 +1,36 @@
+/* global require, process */
+const test = require('ava');
+const path = require('path');
+const fs = require('fs');
+const {generateResultFile} = require('../lib/utils/generate-result-file');
+
+const config = {
+  diffDirectory: `${process.cwd()}`,
+  quality: 'png',
+  threshold: 0.2,
+  baseUrl: 'http://test.com',
+  dimensions: {
+    desktop: {
+      height: 300,
+      width: 200
+    }
+  }
+};
+
+const results = {
+  tests: "generate-result-file.test.js",
+  failures: "none"
+};
+
+test('generateResultFile - null', t => {
+  t.throws(() => generateResultFile());
+});
+
+test('generateResultFile', async t => {
+  await generateResultFile(config, results);
+  t.is(fs.existsSync(path.join(process.cwd(), 'results.json')), true);
+});
+
+test.after('cleanup', () => {
+  fs.unlink(path.join(process.cwd(), 'results.json'), () => console.log('Test file deleted'));
+});
