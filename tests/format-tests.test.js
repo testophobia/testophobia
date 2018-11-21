f20f8780492df0bfe99b22077949125bea1f29f6
srmward <srmward@gmail.com>
2018-11-21 14:18:59 -0800

    Fix format-tests Test Scripts

--- tests/format-tests.test.js
+++ tests/format-tests.test.js
@@ -2,12 +2,10 @@
 const test = require('ava');
 const {formatTests} = require('../lib/utils/format-tests');
 
-const mockConfig = {
-
-};
+const path = "examples/basic/tests/about/about-test.js";
 
 const arrTarget = [
-  "../examples/basic/tests/about/about-test.js"
+  path
 ];
 
 test('formatTest - empty array', async t => {
@@ -16,6 +14,13 @@ test('formatTest - empty array', async t => {
 });
 
 test('formatTest - array', async t => {
-  let r = await formatTests(mockConfig, arrTarget);
-  t.is(r, 0);
+  let r = await formatTests({}, arrTarget);
+  t.is(r.length && r[0].hasOwnProperty('name'), true);
+  t.is(Array.isArray(r), true);
+});
+
+test('formatTest - string', async t => {
+  let r = await formatTests({}, path);
+  t.is(r.length && r[0].hasOwnProperty('name'), true);
+  t.is(Array.isArray(r), true);
 });
