da29b5a1d0cf778241a5335c706c0a8aad055318
srmward <srmward@gmail.com>
2018-11-21 15:21:36 -0800

    Add load-config Units

---
+++ tests/load-config.test.js
@@ -0,0 +1,28 @@
+/* global require, process */
+const test = require('ava');
+const {loadConfig} = require('../lib/utils/load-config');
+
+const passedDefaults = {
+  diffDirectory: `${process.cwd()}`,
+  quality: 'png',
+  threshold: 0.2,
+  baseUrl: 'http://test.com',
+  dimensions: {
+    desktop: {
+      height: 300,
+      width: 200
+    }
+  }
+};
+
+test('loadConfig - null, without default', async t => {
+  let i = await loadConfig();
+  t.is(typeof(i), 'object');
+  t.is(i.hasOwnProperty('baseUrl'), true);
+});
+
+test('loadConfig', async t => {
+  let i = await loadConfig(passedDefaults);
+  t.is(typeof(i), 'object');
+  t.is(i.hasOwnProperty('baseUrl'), true);
+});
