310c61dd62e68ec8257b47213965f9a3b5e0bc26
srmward <srmward@gmail.com>
2018-11-26 10:40:14 -0800

    ScreenBase Units

---
+++ tests/ScreenBase.test.js
@@ -0,0 +1,33 @@
+/* global require, process */
+const test = require('ava');
+const {ScreenBase} = require('../lib/ScreenBase');
+
+const path = "examples/basic/tests/about/about-test.js";
+
+const config = {
+  diffDirectory: `${process.cwd()}`,
+  goldenDirectory: `${process.cwd()}`,
+  testDirectory: `${process.cwd()}`,
+  quality: 'jpeg',
+  threshold: 0.2,
+  baseUrl: 'http://test.com',
+  dimensions: {
+    desktop: {
+      height: 300,
+      width: 200
+    }
+  },
+  tests: path
+};
+
+test('ScreenBase init - no args', t => {
+  t.throws(() => new ScreenBase());
+});
+
+test('ScreenBase init - args', async t => {
+  let c = await new ScreenBase(config);
+  t.is(c.isGolden === false, true);
+  t.is(c.hasOwnProperty('goldenDirectory') && c.goldenDirectory === process.cwd(), true);
+  t.is(c.hasOwnProperty('diffDirectory') && c.diffDirectory === process.cwd(), true);
+  t.is(c.hasOwnProperty('testDirectory') && c.testDirectory === process.cwd(), true);
+});
