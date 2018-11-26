1b648622eeb4695198298e995c64472ec9edf9dd
srmward <srmward@gmail.com>
2018-11-26 10:16:18 -0800

    Configuration Units

---
+++ tests/Configuration.test.js
@@ -0,0 +1,40 @@
+/* global require, process */
+const test = require('ava');
+const {Configuration} = require('../lib/Configuration');
+
+const path = "examples/basic/tests/about/about-test.js";
+
+const config = {
+  diffDirectory: `${process.cwd()}`,
+  quality: 'jpeg',
+  threshold: 0.2,
+  baseUrl: 'http://test.com',
+  dimensions: {
+    desktop: {
+      height: 300,
+      width: 200
+    }
+  },
+  tests: path
+};
+
+test('Configuration init - no args', async t => {
+  let c = await new Configuration();
+  t.is(c.target === 'undefined', true);
+  t.is(typeof(c.config) === 'object', true);
+  t.is(c.config.hasOwnProperty('goldenDirectory'), true);
+  t.is(c.config.hasOwnProperty('diffDirectory'), true);
+  t.is(c.config.hasOwnProperty('testDirectory'), true);
+});
+
+test('Configuration init - args', async t => {
+  let c = await new Configuration(config);
+  t.is(c.target === path, true);
+  t.is(typeof(c.config) === 'object', true);
+  t.is(c.config.hasOwnProperty('goldenDirectory'), true);
+  t.is(c.config.hasOwnProperty('diffDirectory'), true);
+  t.is(c.config.hasOwnProperty('testDirectory'), true);
+  t.is(c.config.hasOwnProperty('quality'), true);
+  t.is(c.config.quality === 'jpeg', true);
+  t.is(c.cli.hasOwnProperty('flags') && c.cli.flags.bail === false, true);
+});
