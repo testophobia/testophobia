5c9b8b91260e1ca125e0b6e1d378777dfc4327e8
srmward <srmward@gmail.com>
2018-11-28 11:14:53 -0800

    Refactor Script

--- tests/common/generate-temp.js
+++ tests/common/generate-temp.js
@@ -2,4 +2,4 @@
 const path = require('path');
 const {createDirectory} = require('../../lib/utils');
 
-(() => createDirectory(path.resolve(process.cwd(), 'tests/temp')))();
+createDirectory(path.resolve(process.cwd(), 'tests/temp'));
