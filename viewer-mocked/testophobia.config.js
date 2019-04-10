338c69ec4e5ceb6830e48b0c2915bdd17380f7ea
Jason Gardner <jason.gardner.lv@gmail.com>
2019-04-10 16:34:46 -0700

    some initial tests against the viewer.  had to introduce a new drag action, which needs to be added throughout

---
+++ viewer-mocked/testophobia.config.js
@@ -0,0 +1,6 @@
+export default {
+  testDirectory: './mock-results/test-screens',
+  goldenDirectory: './mock-results/golden-screens',
+  diffDirectory: './mock-results/diffs',
+  fileType: 'jpeg'
+};
