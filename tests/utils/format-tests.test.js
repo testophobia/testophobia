ea827b32e86938f6a1705f8486fe57d9ca10f80d
srmward <srmward@gmail.com>
2019-04-15 15:43:11 -0700

    Enhance format-tests Tests

--- tests/utils/format-tests.test.js
+++ tests/utils/format-tests.test.js
@@ -1,13 +1,13 @@
 /* global require */
 const test = require('ava');
 const {formatTests} = require('../../lib/utils/format-tests');
-const {testPath} = require('../common/config');
+const {testPath, exampleTest} = require('../common/config');
 
 const arrTarget = [
   testPath
 ];
 
-test('formatTest - empty array', async t => {
+test('formatTest - no config', async t => {
   let r = await formatTests(null, []);
   t.is(r, 0);
 });
@@ -16,6 +16,24 @@ test('formatTest - array', async t => {
   let r = await formatTests({}, arrTarget);
   t.is(r.length && r[0].hasOwnProperty('name'), true);
   t.is(Array.isArray(r), true);
+});
+
+test('formatTest - empty array', async t => {
+  let r = await formatTests({}, []);
+  t.is(r, 0);
+});
+
+test('formatTest - undefined target', async t => {
+  let tgt;
+  let r = await formatTests({}, tgt);
+  t.is(r, 0);
+});
+
+test('formatTest - test object passed', async t => {
+  let r = await formatTests({tests: exampleTest}, exampleTest);
+  t.is(r.length, 1);
+  t.is(r[0].name, 'home');
+  t.is(r[0].actions.length, 1);
 });
 
 test('formatTest - string', async t => {
