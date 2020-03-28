9368d7c46e9040dcff7a4bb33ab73d2274887133
Jason Gardner <jason.gardner.lv@gmail.com>
2020-03-28 12:43:37 -0700

    published 0.9.7 - downgrade Puppeteer due to file chooser bug, small formatting change to golden viewer for ease of use

--- viewer/goldenlist.js
+++ viewer/goldenlist.js
@@ -1,39 +1,47 @@
 /* global $, Testophobia */
 (() => {
-
-function fetchGoldenDirs() {
-  return new Promise(resolve => {
-    $.getJSON('golden-dirs', d => {
-      Testophobia.goldenDirs = d;
-      buildList();
-      resolve();
+  function fetchGoldenDirs() {
+    Testophobia.testNames = {};
+    return new Promise(resolve => {
+      $.getJSON('golden-dirs', d => {
+        d.forEach(t => {
+          const idx = t.indexOf('/');
+          const testDim = t.substr(0, idx);
+          const testName = t.substr(idx + 1);
+          if (!Testophobia.testNames[testName]) Testophobia.testNames[testName] = [];
+          Testophobia.testNames[testName].push(testDim);
+        });
+        buildList();
+        resolve();
+      });
     });
-  });
-}
+  }
 
-async function showListView() {
-  $('#golden-view').show();
-  $('#lnk-start-over').hide();
-  $('#viewer-view').hide();
-  await fetchGoldenDirs();
-}
+  async function showListView() {
+    $('#golden-view').show();
+    $('#lnk-start-over').hide();
+    $('#viewer-view').hide();
+    await fetchGoldenDirs();
+  }
 
-function buildList() {
-  $('#golden-list').empty();
-  Testophobia.goldenDirs.forEach(g => {
-    const thisDir = $(`<li><div>${g}</div></li>`);
-    thisDir.click(() => {
-      Testophobia.viewer.init(true, g);
+  function buildList() {
+    $('#golden-list').empty();
+    Object.keys(Testophobia.testNames).forEach(k => {
+      const thisRow = $(`<li></li>`);
+      thisRow.append($(`<div>${k}</div>`));
+      Testophobia.testNames[k].forEach(d => {
+        const dimBtn = $(`<div>${d}</div>`);
+        dimBtn.click(() => Testophobia.viewer.init(true, `${d}/${k}`));
+        thisRow.append(dimBtn);
+      });
+      $('#golden-list').append(thisRow);
     });
-    $('#golden-list').append(thisDir);
-  });
-}
+  }
 
-async function init() {
-  await showListView();
-  $('#lnk-start-over').click(showListView);
-}
+  async function init() {
+    await showListView();
+    $('#lnk-start-over').click(showListView);
+  }
 
-Testophobia.goldenlist = {init:init};
+  Testophobia.goldenlist = {init: init};
 })();
-
