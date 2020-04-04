f85eb5c93d2afbbaaa705d773bd2e117acde9fff
Jason Gardner <jason.gardner.lv@gmail.com>
2020-04-04 12:55:58 -0700

    finished grouping view goldens

--- viewer/goldenlist.js
+++ viewer/goldenlist.js
@@ -1,16 +1,25 @@
 /* global $, Testophobia */
 (() => {
   function fetchGoldenDirs() {
-    Testophobia.testNames = {};
+    Testophobia.testGoldens = {};
+    const testDims = {};
     return new Promise(resolve => {
       $.getJSON('golden-dirs', d => {
-        d.forEach(t => {
-          console.dir(t);
-          const idx = t.indexOf('/');
-          const testDim = t.substr(0, idx);
-          const testName = t.substr(idx + 1);
-          if (!Testophobia.testNames[testName]) Testophobia.testNames[testName] = [];
-          Testophobia.testNames[testName].push(testDim);
+        const grouped = d.reduce((dict, item) => {
+          if (!dict[item.testCategory]) dict[item.testCategory] = [];
+          if (!testDims[item.testCategory]) testDims[item.testCategory] = [];
+          const idx = item.golden.indexOf('/');
+          const testDim = item.golden.substr(0, idx);
+          item.golden = item.golden.substr(idx + 1);
+          item.dimensions = [];
+          testDims[item.testCategory].push({golden: item.golden, dimension: testDim});
+          if (!dict[item.testCategory].find(g => g.golden === item.golden)) dict[item.testCategory].push(item);
+          return dict;
+        }, Testophobia.testGoldens);
+        Object.keys(testDims).forEach(k => {
+          testDims[k].forEach(td => {
+            Testophobia.testGoldens[k].find(tg => tg.golden === td.golden).dimensions.push(td.dimension);
+          });
         });
         buildList();
         resolve();
@@ -28,16 +37,19 @@
   function buildList() {
     $('#golden-view').empty();
     let thisList;
-    Object.keys(Testophobia.testNames).forEach(k => {
+    Object.keys(Testophobia.testGoldens).forEach(k => {
+      $('#golden-view').append($(`<div class="golden-list-section">${k}</div>`));
       thisList = $('<ul class="golden-list"></ul>');
-      const thisRow = $(`<li></li>`);
-      thisRow.append($(`<div>${k}</div>`));
-      Testophobia.testNames[k].forEach(d => {
-        const dimBtn = $(`<div>${d}</div>`);
-        dimBtn.click(() => Testophobia.viewer.init(true, `${d}/${k}`));
-        thisRow.append(dimBtn);
+      Testophobia.testGoldens[k].forEach(tg => {
+        const thisRow = $(`<li></li>`);
+        thisRow.append($(`<div>${tg.golden}</div>`));
+        tg.dimensions.forEach(d => {
+          const dimBtn = $(`<div>${d}</div>`);
+          dimBtn.click(() => Testophobia.viewer.init(true, `${d}/${tg.golden}`));
+          thisRow.append(dimBtn);
+        });
+        thisList.append(thisRow);
       });
-      thisList.append(thisRow);
       $('#golden-view').append(thisList);
     });
   }
