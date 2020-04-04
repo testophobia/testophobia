e7efb31f7da89b0626c7f17f39f1fbcbd7b4ab9e
Jason Gardner <jason.gardner.lv@gmail.com>
2020-04-04 15:11:10 -0700

    viewer test fixes

--- viewer-mocked/testophobia.config.js
+++ viewer-mocked/testophobia.config.js
@@ -2,5 +2,6 @@ export default {
   testDirectory: './mock-results/test-screens',
   goldenDirectory: './mock-results/golden-screens',
   diffDirectory: './mock-results/diffs',
+  tests: './mock-results/tests/**/*-test.js',
   fileType: 'jpeg'
 };
